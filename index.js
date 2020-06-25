b70e0aae2e27586f3d7d791e581775d8ed11dc70
Alexis Rodriguez <alexis.rodriguez@correounivalle.edu.co>
2020-06-25 17:33:15 -0500

    confi cors

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const routes = require('./routes');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+//CORS permite que un cliente se conecte a otro servidor para el intercambio de recursos
+const cors = require('cors');
+
 //conectar mongo
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/restapis', {
@@ -16,6 +19,10 @@ const app = express();
 //habilita bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+//Habilitar cors
+app.use(cors());
+
 
 //Rutas de la app
 app.use('/', routes());
